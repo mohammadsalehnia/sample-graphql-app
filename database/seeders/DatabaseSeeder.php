13f68efb31d9a4242b5b0dd551390dbb0ab038ea
Mohammad Salehnia <12606608+mohammadsalehnia@users.noreply.github.com>
2024-09-05 19:12:17 +0330

    install telescope add user.graphql and post.graphql

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Post;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Database\Factories\PostFactory;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -15,9 +17,15 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+//        User::factory()->create([
+//            'name' => 'Test User',
+//            'email' => 'test@example.com',
+//        ]);
+
+        $user = User::first();
+
+        Post::factory()->count(3)->create(['user_id' => $user->id]);
+
+
     }
 }
